1656837b2795e10e274274346713ed2a9289f903
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-01-22 21:42:50 +0200

    Updates

---
+++ precache-manifest.4381390326ba338bc7d3cb23bebad70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a961a7888cf31b1adbfe06c6fae6e862",
+    "url": "/swapi-wiki/index.html"
+  },
+  {
+    "revision": "14b0f2c293f193240601",
+    "url": "/swapi-wiki/static/css/main.800700ed.chunk.css"
+  },
+  {
+    "revision": "c108455def37c584d6f4",
+    "url": "/swapi-wiki/static/js/2.7e1b2d46.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/swapi-wiki/static/js/2.7e1b2d46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14b0f2c293f193240601",
+    "url": "/swapi-wiki/static/js/main.fa7eab98.chunk.js"
+  },
+  {
+    "revision": "01e620bcfb0e95b8c148",
+    "url": "/swapi-wiki/static/js/runtime-main.88c466a3.js"
+  }
+]);
